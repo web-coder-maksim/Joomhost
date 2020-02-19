4b08956a89317cb733bdbd878b58c8376ae851d4
Dolph__lundgren <gulivion@mail.ru>
2020-02-19 21:59:38 +0300

    joohost folder

---
+++ js/main.js
@@ -0,0 +1,28 @@
+$(function(){
+
+
+    $('.reviews-slider').slick({
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        arrows: false,
+        dots: true,
+        responsive: [
+            {
+              breakpoint: 1106,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+              }
+            },
+        ]
+    });
+
+    $('.btn__menu').on('click', function(){
+        $('.menu__list').slideToggle();
+    });
+
+
+});
